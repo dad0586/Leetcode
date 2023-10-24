33b81e1fc7585e27157af3d9557b88fdacfbd0b9
OTABOY RAKHMONOV <120033884+dad0586@users.noreply.github.com>
2023-10-24 16:03:25 +0500

    math50l

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public class Main {
+
+
+        public static void main(String[] args) {
+//            Leetcode1 leetcode1 = new Leetcode1();
+//            String s = "([{}])";
+//            boolean valid = Leetcode1.isValid(s);
+//            System.out.println( valid);
+            Math50 math50 = new Math50();
+            System.out.println(math50.myPow(2.00000,10));
         }
+
     }
-}
+
---
+++ src/Math50.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Math50 {
+    public double myPow(double x, int n) {
+double sol =Math.pow(x,n);
+
+        return  sol;
+    }
+
+}
